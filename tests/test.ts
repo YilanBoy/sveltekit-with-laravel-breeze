e5cfc8f478423ce8385c3e7c66e0027b263f524f
Allen Jiang <allenjianggo@gmail.com>
2024-06-19 17:32:20 +0800

    test: add welcome page test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,47 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('welcome page can be visited', async ({ page }) => {
+	const response = await page.request.get('/');
+
+	await expect(response).toBeOK();
+});
+
+test('welcome page has a heading', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+
+	await expect(
+		page.getByRole('heading', { name: '歡迎來到 SvelteKit & Laravel 範例' })
+	).toBeVisible();
+});
+
+test('welcome page has a login page link', async ({ page }) => {
+	await page.goto('/');
+
+	await expect(page.getByRole('link', { name: '登入' })).toBeVisible();
+});
+
+test('welcome page has a register page link', async ({ page }) => {
+	await page.goto('/');
+
+	await expect(page.getByRole('link', { name: '註冊' })).toBeVisible();
+});
+
+test('redirect to login page', async ({ page }) => {
+	await page.goto('/');
+
+	await page.getByRole('link', { name: '登入' }).click();
+
+	await page.waitForURL('/login');
+
+	expect(page.url()).toContain('/login');
+});
+
+test('redirect to register page', async ({ page }) => {
+	await page.goto('/');
+
+	await page.getByRole('link', { name: '註冊' }).click();
+
+	await page.waitForURL('/register');
+
+	expect(page.url()).toContain('/register');
 });
