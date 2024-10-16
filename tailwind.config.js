329a52fa215ff6046050c46486cc7d35f3fd2431
Allen Jiang <allenjianggo@gmail.com>
2024-10-16 18:16:19 +0800

    build: update npm packages and eslint config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
-	plugins: [require('@tailwindcss/forms')]
+	plugins: [forms]
 };
